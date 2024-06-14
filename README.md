f7833ff99c0737c9773521fa206e845fdbfd4463
ritik <flipit001@gmail.com>
2024-06-13 20:48:02 -0700

    added code and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
+
 # Auto-Pip-Install
-Automatically install all needed dependencies for inputted python file(s).
+Automatically install all needed dependencies from inputted python file(s).
+
+# Install
+```bash
+> git clone git@github.com:flipit001/Auto-Pip-Install.git
+> pip3 install -r requirements.txt
+```
+# Use
+```bash
+> python3 auto_pip_install.py {CONFIG JSON FILE}
+```
+## Example
+```bash
+> cat example.json
+[
+    "test.py",
+    "test2.py",
+    "test3.py"
+]
+> python3 auto_pip_install.py example.json
+```
+```json config file```: An array of strings to the path of files from which you want to install the needed dependencies.
---
+++ auto_pip_install.py
@@ -0,0 +1,60 @@
+import subprocess
+import requests
+import re
+import ast
+import json
+import sys
+
+PIP_SEARCH = re.compile(r"\<div\sclass\=\"[^\"]*\"\>pypi\.org\s[^\s]*\sproject\s[^\s]*\s([^\<]*)\<\/div\>")
+
+def have_dependency(dependency: str) -> bool:
+    try:
+        exec(f"import {dependency}")
+
+    except ImportError:
+        return False
+
+    return True
+
+def install_dependency(dependency: str) -> None:
+    url = f"https://www.google.com/search?q={dependency}+pypi"
+    try:
+        cmd = PIP_SEARCH.findall(requests.get(url).content.decode(errors="ignore"))[0]
+    
+    except IndexError:
+        print(f"failed to install dependency: {dependency}")
+        return
+
+    pip_command = subprocess.Popen(["pip3", "install", cmd], stdout=subprocess.PIPE)
+    pip_command.wait()
+    print(f"Successfully installed dependency {dependency}")
+
+def install_dependencies_from_files(files: list[str]):
+    dependencies = []
+    for file in files:
+        with open(file, "r") as fh:
+            code = fh.read()
+
+        ast_representation = ast.parse(code)
+        for block in ast_representation.body:
+            if isinstance(block, ast.Import):
+                for import_ in block.names:
+                    dependencies.append(import_.name)
+            
+            if isinstance(block, ast.ImportFrom):
+                dependencies.append(block.module)
+        
+    dependencies = list(set(dependencies))
+    for dependency in dependencies:
+        if not have_dependency(dependency):
+            install_dependency(dependency)
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("PLEASE SPECIFY THE CONFIG FILE: python3 auto_pip_install.py {CONFIG JSON FILE}")
+        exit(1)
+
+    with open(sys.argv[1], "r") as fh:
+        files = json.load(fh)
+
+    install_dependencies_from_files(files)
